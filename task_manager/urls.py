c3cfe76c468043b1ada5c2434383dedbfd1bfd8c
Mohammed Rabil <rabil@techie.com>
2022-01-30 22:56:22 +0530

    Final Submission

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 
+from tasks.views import index, add_task, delete_task, complete_task, tasks, completed_tasks, delete_completed_task, all_tasks
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", index, name="index"),
+    path("add-task/", add_task, name="add-task"),
+    path("delete-task/<int:task_id>/", delete_task, name="delete-task"),
+    path("complete_task/<int:task_id>/", complete_task, name="complete-task"),
+    path("tasks/", tasks, name="tasks"),
+    path("completed_tasks/", completed_tasks, name="completed-tasks"),
+    path("delete-completed-task/<int:task_id>/", delete_completed_task, name="delete-completed-task"),
+    path("all_tasks/", all_tasks, name="all-tasks"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,73 @@
+from django.shortcuts import render, redirect
+
 # Add your Views Here
+from tasks.models import Task
+
+
+# Create your views here.
+def index(request):
+    return redirect('tasks')
+# Add a new task
+def add_task(request):
+    # get the task as parameter
+    task = request.GET.get('task')
+    # create a new task
+    new_task = Task(title=task, completed=False)
+    # save the task
+    new_task.save()
+    # redirect to the tasks view
+    return redirect('tasks')
+
+# Delete a task
+def delete_task(request, task_id):
+    # get the task as parameter
+    task = Task.objects.get(id=task_id)
+    # delete the task
+    task.delete()
+    # redirect to the tasks view
+    return redirect('tasks')
+
+# Complete a task
+def complete_task(request, task_id):
+    # get the task as parameter
+    task = Task.objects.get(id=task_id)
+    task.completed = True
+    # save the task
+    task.save()
+    # redirect to the tasks view
+    return redirect('tasks')
+
+# View all tasks
+def tasks(request):
+    search_term = request.GET.get('search')
+    if search_term:
+        tasks = Task.objects.filter(title__icontains=search_term, completed=False)
+    else:
+        tasks = Task.objects.filter(completed=False)
+    # render the tasks
+    return render(request, 'tasks.html', {'tasks': tasks})
+
+# View all completed tasks
+def completed_tasks(request):
+    # get all completed tasks
+    completed_tasks = Task.objects.filter(completed=True).order_by('-created_date')
+    # render the completed tasks
+    return render(request, 'completed_tasks.html', {'tasks': completed_tasks})
+
+# delete  completed task
+def delete_completed_task(request, task_id):
+    # get the task as parameter
+    task = Task.objects.get(id=task_id)
+    # delete the task
+    task.delete()
+    # redirect to the tasks view
+    return redirect('completed-tasks')
+
+# view all tasks and completed tasks
+def all_tasks(request):
+    # get all tasks
+    tasks = Task.objects.filter(completed=False)
+    # get all completed tasks
+    completed_tasks = Task.objects.filter(completed=True).order_by('-created_date')
+    # render the tasks and completed tasks
+    return render(request, 'all_tasks.html', {'tasks': tasks, 'completed_tasks': completed_tasks})
